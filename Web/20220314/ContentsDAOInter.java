924fe978b1c39eb33c2789aca68ba73a608842ac
CHAEYOUNG <84116509+mooncy0809@users.noreply.github.com>
2022-03-14 21:00:21 +0900

    Add files via upload

---
+++ Web/20220314/ContentsDAOInter.java
@@ -0,0 +1,53 @@
+package dev.mvc.contents;
+
+import java.util.HashMap;
+import java.util.List;
+
+public interface ContentsDAOInter {
+  /**
+   * 등록
+   * @param contentsVO
+   * @return
+   */
+  public int create(ContentsVO contentsVO);
+  
+  /**
+   * 조회
+   * @param contentsno
+   * @return
+   */
+  public ContentsVO read(int contentsno);
+  /**
+   * 상품 정보 수정 처리
+   * @param contentsVO
+   * @return
+   */
+  public int product_update(ContentsVO contentsVO);
+  
+  /**
+   * 특정 카테고리의 등록된 글목록
+   * @return
+   */
+  public List<ContentsVO> list_by_cateno(int cateno);
+  
+  /**
+   * 카테고리별 검색 목록
+   * @param hashMap
+   * @return
+   */
+  public List<ContentsVO> list_by_cateno_search(HashMap<String, Object> hashMap);
+
+  /**
+   * 카테고리별 검색 레코드 갯수
+   * @param hashMap
+   * @return
+   */
+  public int search_count(HashMap<String, Object> hashMap);
+  
+  /**
+   * 검색 + 페이징 목록
+   * @param map
+   * @return
+   */
+  public List<ContentsVO> list_by_cateno_search_paging(HashMap<String, Object> map);
+}
